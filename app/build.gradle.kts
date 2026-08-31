93efe719899814a91960813cf15da794c8db4410
zhaojin0614 <3194359418@qq.com>
2026-08-31 21:47:59 +0800

    chore(release): versionName 2.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "2.2.4"
+        versionName = "2.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
