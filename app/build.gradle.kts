dbb312b744c32d4f948bc80dee0cc26e92eef4a8
zhaojin0614 <3194359418@qq.com>
2026-09-02 02:34:06 +0800

    chore(app): 更新应用版本号至2.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "2.2.6"
+        versionName = "2.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
